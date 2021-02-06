6cc6c8ae0bafd405135b71d6ee55de5546523eb6
Vitaliy <Betalik1997@gmail.com>
2021-02-06 11:07:13 +0300

    Добавил комментарии состояний

--- states.py
+++ states.py
@@ -4,12 +4,12 @@ from aiogram.utils.helper import Helper, HelperMode, Item
 class BotStates(Helper):
     mode = HelperMode.snake_case
 
-    START_STATE = Item()
-    STYLE_TRANSFER_STATE_1 = Item()
-    STYLE_TRANSFER_STATE_2 = Item()
-    STYLE_GAN_STATE = Item()
-    CUBISM_STATE = Item()
-    RENAISSANCE_STATE = Item()
-    EXPRESSIONISM_STATE = Item()
-    SUPER_RESOLUTION_STATE = Item()
+    START_STATE = Item()                # начальное состояние
+    STYLE_TRANSFER_STATE_1 = Item()     # ожидание изображения стиля для NST
+    STYLE_TRANSFER_STATE_2 = Item()     # ожидание изображения контента для NST
+    STYLE_GAN_STATE = Item()            # ожидание выбора стиля для CycleGAN
+    CUBISM_STATE = Item()               # ожидание изображения для CycleGAN
+    RENAISSANCE_STATE = Item()          # ожидание изображения для CycleGAN
+    EXPRESSIONISM_STATE = Item()        # ожидание изображения для CycleGAN
+    SUPER_RESOLUTION_STATE = Item()     # ожидание изображения для ESRGAN
 
